fa881a557989c63121e29e4d00556da9c034fd9b
Thomas Watson Steen <w@tson.dk>
2014-07-24 11:52:41 +0200

    Allow the on-request event listener to be parsed when creating the server

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var port = process.env.PORT = process.env.PORT || 5000;
 var http = require('http');
 var airserver = require('./lib/airserver');
 
-module.exports = function () {
-  var server = http.createServer();
+module.exports = function (onRequest) {
+  var server = http.createServer(onRequest);
   server.on('listening', function () {
     airserver(server.address().port);
   });
