2524936df4bbc930ecf6fc8dc45869fc0b18ff4b
Thomas Watson Steen <w@tson.dk>
2014-07-26 16:31:40 +0200

    Move RAOP and AirPlay servers into seperate modules

--- examples/server.js
+++ examples/server.js
@@ -1,21 +1,64 @@
 'use strict';
 
+var bplist = require('bplist-parser');
+var gm = require('graphicsmagick-stream');
+var pictureTube = require('picture-tube');
+
 var airserver = require('../')();
 
-airserver.on('request', function (req, res) {
-  console.log('Processing incoming request...');
-  var buffers = [];
-  req.on('data', buffers.push.bind(buffers));
-  req.on('end', function () {
-    res.end();
-    console.log(req.method, req.url);
-    console.log(req.headers);
-    if (buffers.length) console.log(Buffer.concat(buffers).toString());
-  });
+var getServerInfo = function (req, res) {
+  var mac = 'B8:8D:12:14:43:D4';
+  var features = 268438015;
+  var model = 'AppleTV3,1';
+  var protovers = '1.0';
+  var srcvers = '150.33';
+
+  var body = '<?xml version="1.0" encoding="UTF-8"?>\n' +
+             '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"\n' +
+             ' "http://www.apple.com/DTDs/PropertyList-1.0.dtd">\n' +
+             '<plist version="1.0">\n' +
+             ' <dict>\n' +
+             '  <key>deviceid</key>\n' +
+             '  <string>' + mac + '</string>\n' +
+             '  <key>features</key>\n' +
+             '  <integer>' + features + '</integer>\n' +
+             '  <key>vv</key>\n' +
+             '  <string>1</string>\n' +
+             '  <key>rhd</key>\n' +
+             '  <string>1.06.5</string>\n' +
+             '  <key>pw</key>\n' +
+             '  <string>0</string>\n' +
+             '  <key>srcvers</key>\n' +
+             '  <string>' + srcvers + '</string>\n' +
+             '  <key>rmodel</key>\n' +
+             '  <string>MacBookair4,2</string>\n' +
+             '  <key>model</key>\n' +
+             '  <string>' + model + '</string>\n' +
+             '  <key>protovers</key>\n' +
+             '  <string>' + protovers + '</string>\n' +
+             ' </dict>\n' +
+             '</plist>';
+
+  res.setHeader('Content-Type', 'text/x-apple-plist+xml');
+  res.setHeader('Content-Length', body.length);
+  res.end(body);
+};
+
+var putPhoto = function (req, res) {
+  var tube = pictureTube();
+  tube.pipe(process.stdout);
+  var toPng = gm({ format: 'png' });
+  req.pipe(toPng()).pipe(tube);
+};
+
+airserver.on('airplay', function (req, res) {
+  switch (req.url) {
+    case '/photo':
+      putPhoto(req, res);
+      break;
+    default:
+      res.end();
+  }
 });
 
-console.log('Starting AirServer...');
-airserver.listen(function () {
-  var port = airserver.address().port;
-  console.log('AirServer listening on port', port);
-});
+airserver.listen();
