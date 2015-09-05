a702e3bd26f020e7a24939024b522ad81e18a254
Thomas Watson Steen <w@tson.dk>
2015-09-06 01:05:50 +0200

    Add custom server settings
    These have been lifted from http://nto.github.io/AirPlay.html

--- examples/server.js
+++ examples/server.js
@@ -1,9 +1,13 @@
 'use strict';
 
+// hack to ensure that we get a new mac-address every time to avoid that the
+// settings are cached on the phone
+global.mac = String(Math.random()).substr(2, 12).match(/../g).join(':');
+
 var gm = require('graphicsmagick-stream');
 var pictureTube = require('picture-tube');
 
-var airserver = require('../')('Picture Frame');
+var airserver = require('../')('Picture Frame ' + global.mac.substr(0, 5));
 
 airserver.on('photo', function (stream) {
   var tube = pictureTube();
